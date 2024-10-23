6dc89273af6869919292b3b39aef1ec2ba0681fd
Nacht. <putramadillah@gmail.com>
2024-10-23 09:38:44 +0700

    home_page.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,8 @@
-// lib/screens/home_page.dart
 import 'package:flutter/material.dart';
 import '../models/peminjaman.dart';
 import 'input_peminjaman.dart';
 import 'daftar_peminjaman.dart';
+import 'detail_peminjaman.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -37,6 +37,7 @@ class _HomePageState extends State<HomePage> {
               ),
             ),
             ListTile(
+              leading: const Icon(Icons.add),
               title: const Text('Input Peminjaman'),
               onTap: () {
                 Navigator.pop(context);
@@ -55,6 +56,7 @@ class _HomePageState extends State<HomePage> {
               },
             ),
             ListTile(
+              leading: const Icon(Icons.list),
               title: const Text('Daftar Peminjaman'),
               onTap: () {
                 Navigator.pop(context);
@@ -71,8 +73,55 @@ class _HomePageState extends State<HomePage> {
           ],
         ),
       ),
-      body: const Center(
-        child: Text('Welcome To Bank Adillah'),
+      body: Center(
+        child: daftarPinjaman.isEmpty
+            ? const Text(
+                'Belum ada data peminjaman',
+                style: TextStyle(fontSize: 18),
+              )
+            : ListView.builder(
+                itemCount: daftarPinjaman.length,
+                itemBuilder: (context, index) {
+                  final peminjaman = daftarPinjaman[index];
+                  return Card(
+                    margin: const EdgeInsets.all(8.0),
+                    child: ListTile(
+                      title: Text(peminjaman.namaNasabah),
+                      subtitle: Text(
+                        'Pinjaman: Rp${peminjaman.jumlahPinjaman}',
+                      ),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => DetailPeminjaman(
+                              peminjaman: peminjaman,
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  );
+                },
+              ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => InputPeminjaman(
+                onPeminjamanAdded: (peminjaman) {
+                  setState(() {
+                    daftarPinjaman.add(peminjaman);
+                  });
+                },
+              ),
+            ),
+          );
+        },
+        child: const Icon(Icons.add),
+        tooltip: 'Tambah Peminjaman',
       ),
     );
   }
